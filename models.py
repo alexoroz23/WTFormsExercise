0bc658a847a7d8bd498c3563907bc12982c720b3
alex <alexisoro99@gmail.com>
2023-04-05 23:39:18 -0700

    exercise files

---
+++ models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+
+# Step 1: Create Database & Model
+
+class Pet(db.Model):
+    """Pet Model"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    species = db.Column(db.Text, nullable=False)
+    photo_url = db.Column(db.Text, nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, nullable=False, default=True)
+
+    def image_url(self):
+        """Return image for pet -- bespoke or generic."""
+
+        return self.photo_url
+
+def connect_db(app):
+    
+    db.app = app
+    db.init_app(app)
